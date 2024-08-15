f9e55c5d6153cf9412e9510cc0f134a68eb51081
Hamid-2027 <hamidhussain2027@gmail.com>
2024-08-15 10:13:22 -0700

    adding firebase authentication config

--- App.tsx
+++ App.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const App = () => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
-    </SafeAreaView>
+    <View>
+      <Text>Welcome {user.email}</Text>
+    </View>
   );
-};
+}
+
 
 const styles = StyleSheet.create({
   container: {
