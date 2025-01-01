6c15023396c8875e0a8630e5626306ba32554c98
Hamid-2027 <hamidhussain2027@gmail.com>
2025-01-01 20:24:12 +0500

    update app.tsx

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import WelcomeScreen from './src/screens/welcome-screen';
 import { configureGoogleSignIn } from './src/configureGoogleSignIn';
 import { useSelector } from 'react-redux';
 import PostScreen from './src/screens/post-screen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ const App = () => {
   const [userData, setUserData] = useState(null);
   const isAppReady = useSelector((state) => state.userAuth.isAppReady)
   console.log("isAppReady...",isAppReady)
+  const [isUserLogin, setIsUserLogin] = useState(false);
 
    
   useEffect(() => {
@@ -31,16 +33,29 @@ const App = () => {
     });
 
     return () => getUserLoginDetails();
+
   }, []);
+  console.log("isUserLogin.>>>.",isUserLogin)
+  useEffect(()=>{
+    
+    //................
+    const checkLoginStatus = async () => {
+      const loginStatus = await AsyncStorage.getItem("isUserLogin");
+      console.log("loginStatusKK",loginStatus);
+      setIsUserLogin(JSON.parse(loginStatus));
+    };
+
+    checkLoginStatus();
+  },[isUserLogin,setIsUserLogin])
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {!isAppReady ? (
-          <Stack.Screen name="Auth" component={LoginScreen} />
-        ) : (
+      <Stack.Navigator screenOptions={{ headerShown: false }}
+      initialRouteName=  {!isUserLogin ?'Auth':'welcome' }>
+
+
+        <Stack.Screen name="Auth" component={LoginScreen} />
         <Stack.Screen name="welcome" component={WelcomeScreen} />
-        )}
         <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="Post" component={PostScreen} />
       </Stack.Navigator>
